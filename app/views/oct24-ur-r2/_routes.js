afbf6533882d407153f90edaf397433c8dba860a
domm-edu <dominic.norris@education.gov.uk>
2024-10-31 13:54:47 +0000

    Added additional flows and routing for radios

--- app/views/oct24-ur-r2/_routes.js
+++ app/views/oct24-ur-r2/_routes.js
@@ -3,4 +3,28 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/date-changes', function(request, response) {
+
+    // Is the date you're changing also on GOV.UK?
+
+    var date = request.session.data['dateChanges']
+    if (date == "yes"){
+        response.redirect("published-or-scheduled")
+    } else {
+        response.redirect("cannot-proceed")
+    }
+})
+
+    // Is the change already published or scheduled to be published on GOV.UK?
+
+    router.post('/gov-uk-published-scheduled', function(request, response) {
+
+        var published = request.session.data['publishedScheduled']
+        if (published == "no"){
+            response.redirect("cannot-proceed")
+        } else {
+            response.redirect("review-and-publish")
+        }
+    })
+
 module.exports = router
