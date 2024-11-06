1d07c7599c000e83d3b895f0dd75bf0cd339fd45
domm-edu <dominic.norris@education.gov.uk>
2024-11-06 12:02:48 +0000

    Completed change details flow

--- app/views/oct24-ur-r2/_routes.js
+++ app/views/oct24-ur-r2/_routes.js
@@ -1,45 +1,45 @@
-const express = require('express')  
+const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/date-changes', function(request, response) {
+router.post('/date-changes', function (request, response) {
 
     // Is the date you're changing also on GOV.UK?
 
     var date = request.session.data['dateChanges']
-    if (date == "yes"){
+    if (date == "yes") {
         response.redirect("published-or-scheduled")
     } else {
         response.redirect("cannot-proceed")
     }
 })
 
-    // Is the change already published or scheduled to be published on GOV.UK?
+// Is the change already published or scheduled to be published on GOV.UK?
 
-    router.post('/gov-uk-published-scheduled', function(request, response) {
+router.post('/gov-uk-published-scheduled', function (request, response) {
 
-        var published = request.session.data['publishedScheduled']
-        if (published == "no"){
-            response.redirect("cannot-proceed")
-        } else {
-            response.redirect("review-and-publish")
-        }
-    })
+    var published = request.session.data['publishedScheduled']
+    if (published == "no") {
+        response.redirect("cannot-proceed")
+    } else {
+        response.redirect("review-and-publish")
+    }
+})
 
-     // If a date is selected when a user edits a task
+// If a date is selected when a user edits a task
 
-    router.post('/edit-task-date', (req, res) => { 
-        const startDateSelection = req.body['task-start-date']; 
-        const dueDateSelection = req.body['due-date']; 
-        
-        if (startDateSelection === 'no-start-date' && dueDateSelection === 'no due date') { 
-            res.redirect('check-your-answers'); 
-        } else { 
-            res.redirect('date-change-question-page'); 
-        } 
-    });
+router.post('/edit-task-date', (req, res) => {
+    const startDateSelection = req.body['task-start-date'];
+    const dueDateSelection = req.body['due-date'];
 
-    
+    if (startDateSelection === 'no-start-date' && dueDateSelection === 'no due date') {
+        res.redirect('check-your-answers');
+    } else {
+        res.redirect('date-change-question-page');
+    }
+});
 
-module.exports = router
+
+
+    module.exports = router
