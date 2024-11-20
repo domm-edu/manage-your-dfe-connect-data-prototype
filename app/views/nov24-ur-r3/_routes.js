e9172832e8b56090a41ef3d51346a80162d7b0af
domm-edu <dominic.norris@education.gov.uk>
2024-11-20 13:00:44 +0000

    Added routing for urgent requests

--- app/views/nov24-ur-r3/_routes.js
+++ app/views/nov24-ur-r3/_routes.js
@@ -3,6 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/urgent-request', function(request, response) {
 
+    var publishDate = request.session.data['publish-task-date']
+    if (publishDate == "soon"){
+        response.redirect("create-subtask/urgent")
+    } else {
+        response.redirect("create-subtask/comments")
+    }
+})
 
     module.exports = router
